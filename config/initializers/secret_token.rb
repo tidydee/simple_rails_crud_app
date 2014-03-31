11835bf7719c4aed1ec7f53dc97d381e4404d6aa
dawidspamer <dawidspamer@gmail.com>
2014-03-31 21:33:57 +0000

    Simple Ruby on Rails CRUD App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForum::Application.config.secret_key_base = 'c18b78e81184cfc2f0e9b6601b3e3033640c1243f49c558d7984bce864a6df8180c6bf1bd98995004a6e59e481ba838d428f9079cf0c611c091179e3f658efc7'
